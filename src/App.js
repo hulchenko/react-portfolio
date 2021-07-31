d8e047204e3100432cf9e8149907078c30380b6b
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-07-30 21:33:01 -0600

    add router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import AboutUs from './pages/AboutUs';
 //Styles
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+//Router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
